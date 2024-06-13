660fa4a989f4157d330dbe56a69d975b0298f07e
Awpariq2024 <awpariq@unjbg.edu.pe>
2024-06-13 00:41:26 -0500

    Se agrega el ejercicio 1 de estructuras selectivas

---
+++ Numeros_a_Vocales.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int numero;
+	cout<<"Ingrese un numero natural del 1 al 5: ";
+	cin>>numero;
+	
+	switch (numero){
+		case 1:
+			cout<<"La vocal correspondiente del numero "<<numero<<" es A";
+			break;
+		case 2:
+			cout<<"La vocal correspondiente del numero "<<numero<<" es E";
+			break;
+		case 3:
+			cout<<"La vocal correspondiente del numero "<<numero<<" es I";
+			break;
+		case 4:
+			cout<<"La vocal correspondiente del numero "<<numero<<" es O";
+			break;
+		case 5:
+			cout<<"La vocal correspondiente del numero "<<numero<<" es U";
+			break;
+		default:
+			cout<<"El numero que ingreso no es una opcion valida";
+			break;
+		
+	}
+	
+	return 0;
+}
