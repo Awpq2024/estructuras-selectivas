b14197084f204d8fd63cefa771ce048f6bb40823
Awpq2024 <awpariq@unjbg.edu.pe>
2024-06-15 23:20:01 -0500

    se modifica el ejercicio para repetirse hasta que el numero ingresado sea 0

--- Numeros_a_Vocales.cpp
+++ Numeros_a_Vocales.cpp
@@ -3,31 +3,37 @@ using namespace std;
 
 int main(){
 	int numero;
+	
 	cout<<"------------------Bienvenido-------------"<<endl;
-	cout<<"Ingrese un numero natural del 1 al 5: ";
+	
+	do{
+	cout<<"Ingrese un numero natural del 1 al 5 (0 para salir): ";
 	cin>>numero;
 	
 	switch (numero){
 		case 1:
-			cout<<"La vocal correspondiente del numero "<<numero<<" es A";
+			cout<<"La vocal correspondiente del numero "<<numero<<" es A"<<endl<<endl;
 			break;
 		case 2:
-			cout<<"La vocal correspondiente del numero "<<numero<<" es E";
+			cout<<"La vocal correspondiente del numero "<<numero<<" es E"<<endl<<endl;
 			break;
 		case 3:
-			cout<<"La vocal correspondiente del numero "<<numero<<" es I";
+			cout<<"La vocal correspondiente del numero "<<numero<<" es I"<<endl<<endl;
 			break;
 		case 4:
-			cout<<"La vocal correspondiente del numero "<<numero<<" es O";
+			cout<<"La vocal correspondiente del numero "<<numero<<" es O"<<endl<<endl;
 			break;
 		case 5:
-			cout<<"La vocal correspondiente del numero "<<numero<<" es U";
+			cout<<"La vocal correspondiente del numero "<<numero<<" es U"<<endl<<endl;
+			break;
+		case 0:
+			cout<<"Saliendo del programa...";
 			break;
 		default:
-			cout<<"El numero que ingreso no es una opcion valida";
+			cout<<"El numero que ingreso no es una opcion valida"<<endl<<endl;
 			break;
-		
 	}
+	}while(numero!=0);
 	
 	return 0;
 }
